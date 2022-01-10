eb1078417f5ec43ab3b155005d6adeb3a5dafa35
Ben <bzilber@users.noreply.github.com>
2022-01-10 17:03:40 -0500

    defined filter

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -4,7 +4,7 @@ export default function FilterButton(props) {
     return (
         <button type="button" className="btn toggle-btn" aria-pressed="false">
             <span className="visually-hidden">Show </span>
-            <span>Completed</span>
+            <span>All</span>
             <span className="visually-hidden"> tasks</span>
         </button>
     );
