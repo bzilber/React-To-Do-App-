ceaa3dcd0158a4cb171060a1c6ae27c6a30baf47
Ben <bzilber@users.noreply.github.com>
2022-01-10 20:06:27 -0500

    Filtering Tasks in UI

--- src/App.js
+++ src/App.js
@@ -15,7 +15,29 @@ const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
-  const [filter, setFilter] = UseState('All');
+  const [filter, setFilter] = useState('All');
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task =>
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    );
+
+  const filterList = FILTER_NAMES.map(name => (<FilterButton
+    key={name}
+    name={name}
+    isPressed={name === filter}
+    setFilter={setFilter}
+  />
+  ));
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
@@ -40,17 +62,6 @@ function App(props) {
   function deleteTask(id) {
     setTasks(tasks.filter(task => id !== task.id));
   }
-  const taskList = tasks.map(task =>
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
-  );
 
   function addTask(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
@@ -66,9 +77,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function FilterButton(props) {
     return (
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
+        <button type="button"
+            className="btn toggle-btn"
+            aria-pressed={props.isPressed}
+            onClick={() => props.setFilter(props.name)}
+        >
             <span className="visually-hidden">Show </span>
-            <span>All</span>
+            <span>{props.name}</span>
             <span className="visually-hidden"> tasks</span>
         </button>
     );
